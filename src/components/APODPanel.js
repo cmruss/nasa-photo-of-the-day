60389e3738bb3bd51f8cd07a538ee702f8817481
cmruss <cmrussell08@gmail.com>
2020-03-16 16:39:03 -0400

    favicon and title changed

--- src/components/APODPanel.js
+++ src/components/APODPanel.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef } from "react";
+import React, {useState, useEffect } from "react";
 import APODCard from "./APODCard"
 import axios from "axios";
 import moment from "moment";
