ba2c921aa0df5feefcca95d40690ddf5c5c956df
cmruss <cmrussell08@gmail.com>
2020-03-16 20:02:47 -0400

    added local storage to persist imgDate, initialized state for apod

--- src/components/APODCard.js
+++ src/components/APODCard.js
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
-import { Button } from 'reactstrap';
+import { Button } from "reactstrap";
 import { Jumbotron, Container } from 'reactstrap';
 import styled from "styled-components";
 import moment from "moment";
-import img from "../img/404.jpg"
+import img from "../img/404.jpg";
 
 const ImgCard = styled.div`
     background: dimgrey;
@@ -21,7 +21,7 @@ const AspectRatio = styled.div`
     overflow: hidden;
     border-radius: 50%;
     min-height: 100%;
-`
+`;
 const Img = styled.img`
     overflow:hidden;
     position:absolute;
@@ -40,7 +40,8 @@ const Copyright = styled.p`
 const Credit = styled.p`
     color: dimgrey;
     font-size: 0.8rem;
-`
+    margin-top: 5%;
+`;
 const ButtonContainer = styled.div`
     margin: 1% auto;
     display: flex;
@@ -58,26 +59,51 @@ const APODCard = props => {
       }, [props.date])
 
     return (
-    <ImgCard style={{backgroundImage: props.img ? `url(${props.img})` : `url(${img})` , backgroundRepeat: 'no-repeat', backgroundSize: '100% 100%', display: props.loading ? "none" : "block" }}>
-      <Jumbotron fluid>
-        <Container props fluid>
-        <h1 className="display-4">{props.imgDate}</h1>
-        <AspectRatio>
-            <Img onLoad={props.setLoading(false)} className="apod_img" alt="Picture of the Day" src={props.img ? props.img : img} style={{display: props.loading ? "none" : "block" }}/>
-        </AspectRatio>
-        <TextContainer>
-          <h2 className="lead">{props.title}</h2>
-          <p className="lead">{props.description}</p>
-          {copyright && <Copyright className="lead">©{props.copyright}</Copyright>}
-        </TextContainer>
-        <ButtonContainer>
-            <Button onClick={()=>{props.dateSub(props.date)}} outline color="secondary">previous</Button>
-            <Button onClick={()=>{props.dateAdd(props.date)}} id="Forward" outline color="secondary" disabled={imgDate < today ? false : true }>forward</Button>
-        </ButtonContainer>
-        </Container>
-        <Credit>design 2020 cmruss | made with the NASA APOD API & Material UI</Credit>
-      </Jumbotron>
-    </ImgCard>
+        <ImgCard 
+            style={{
+                backgroundImage: props.img ? `url(${props.img})` : `url(${img})`, 
+                backgroundRepeat: "no-repeat", 
+                backgroundSize: "100% 100%", 
+                display: props.loading ? "none" : "block" 
+            }}
+        >
+            <Jumbotron fluid>
+                <Container props fluid>
+                    <h1 className="display-4">{props.imgDate}</h1>
+                    <AspectRatio>
+                        <Img 
+                            onLoad={props.setLoading(false)} 
+                            className="apod_img" 
+                            alt="Picture of the Day" 
+                            src={props.img ? props.img : img} 
+                            style={{display: props.loading ? "none" : "block" }}
+                        />
+                    </AspectRatio>
+                    <TextContainer>
+                        <h2 className="lead">{props.title}</h2>
+                        <p className="lead">{props.description}</p>
+                        {copyright && <Copyright className="lead">©{props.copyright}</Copyright>}
+                    </TextContainer>
+                    <ButtonContainer>
+                        <Button 
+                            onClick={()=>{props.dateSub(props.date)}} 
+                            outline color="secondary"
+                        >
+                            previous
+                        </Button>
+                        <Button 
+                            onClick={()=>{props.dateAdd(props.date)}} 
+                            id="Forward" 
+                            outline color="secondary" 
+                            disabled={imgDate < today ? false : true }
+                        >
+                            forward
+                        </Button>
+                    </ButtonContainer>
+                </Container>
+                <Credit>design 2020 cmruss | made with the NASA APOD API & Reactstrap</Credit>
+            </Jumbotron>
+        </ImgCard>
     )
 };
 
--- src/components/APODPanel.js
+++ src/components/APODPanel.js
@@ -2,31 +2,40 @@ import React, {useState, useEffect } from "react";
 import APODCard from "./APODCard"
 import axios from "axios";
 import moment from "moment";
+import { useLocalStorage } from '../hooks/useLocalStorage';
+
+let initialState = {
+    copyright: "",
+    date: "",
+    explanation: "",
+    hdurl: "",
+    media_type: "",
+    service_version: "",
+    title: "",
+    url: ""
+}
 
 export default function APODPanel() {
-    const [apod, setApod] = useState([]);
-    const [date,setDate] = useState(Date());
+    const [apod, setApod] = useState(initialState);
+    const [date,setDate] = useLocalStorage("imgDate", Date());
     const [loading, setLoading] = useState(true);
 
-    //make two funtions that increment date +/-, store that increment to state, make a function that converts date to the nasa api format &date=yyyy/mm/dd, recall the current date from state in the get request
     const dateAdd = (date) => {
         var newDate = new Date(date); 
         newDate.setDate(newDate.getDate() + 1);
         setDate(newDate);
-        // console.log("addaday"+ newDate)
     }
     const dateSub = (date) => {
         var newDate = new Date(date);
         newDate.setDate(newDate.getDate() - 1);
         setDate(newDate);
-        // console.log("takaday"+ newDate)
     }
 
     useEffect(() => {
         axios
         .get(`https://api.nasa.gov/planetary/apod?api_key=BcVA8joxv0595Knb0NaCQ4bsU43BTYVKZl3egP6O&date=${moment(date).format("YYYY-MM-DD")}`)
         .then(response => {
-            console.log(response)
+            console.log(response.data)
             setApod(response.data)
         })
         .catch(err => {
