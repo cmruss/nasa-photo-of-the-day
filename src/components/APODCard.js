47bb28b26510c56143901bb5faa760a7d3f70cdd
cmruss <cmrussell08@gmail.com>
2020-03-18 17:27:42 -0400

    formatted start date differently

--- src/components/APODCard.js
+++ src/components/APODCard.js
@@ -74,7 +74,7 @@ const APODCard = props => {
                                 date => {props.setDate(moment(date).format("YYYY-MM-DD"))}}
                             style={{ display: "flex" }}
                             dateFormat={"YYYY-MM-DD"}
-                            maxDate={(props.today)}
+                            // maxDate={(props.today)}
                         />
                     </FormGroup>
                     <ButtonContainer>
