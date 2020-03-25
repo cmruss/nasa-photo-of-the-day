e6c110e7d41f9a671743100baa3c35ee6e6cb906
cmruss <cmrussell08@gmail.com>
2020-03-25 00:52:53 -0400

    left 100%?

--- src/styles/APODCardStyles.js
+++ src/styles/APODCardStyles.js
@@ -55,7 +55,7 @@ export const Video = styled.iframe`
     min-width: 100%;
     object-fit: cover;
     position: absolute;
-    right: 0;
+    left: 100%;
 `
 export const TextContainer = styled.div`
     margin: 5% auto;
